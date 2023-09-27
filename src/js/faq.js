cb704731cb2a4025737d3f8f940d02d39f31c94f
Celine Wan <mwanm074@uottawa.ca>
2023-09-26 23:18:47 -0400

    Updated coaches pages, footer, btn website & faq style

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,9 +1,14 @@
 function handleFAQVisibility(name) {
     var x = document.getElementById(name);
-    if (x.style.display == 'none') {
+    var icon = x.previousElementSibling.querySelector('.icon i');
+
+    if (x.style.display === 'none' || x.style.display === '') {
         x.style.display = 'block';
+        icon.classList.remove('fa-caret-down');
+        icon.classList.add('fa-caret-up');
     } else {
         x.style.display = 'none';
-
+        icon.classList.remove('fa-caret-up');
+        icon.classList.add('fa-caret-down');
     }
 }
